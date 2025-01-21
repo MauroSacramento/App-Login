9abe2dd6b8a0a5c0f0968675993dba6ca045a6e3
Maurosacramento <maurosacramento35@gmail.com>
2025-01-21 18:00:22 +0000

    Styling main-section

---
+++ src/app/components/default-login/default-login.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-default-login',
+  imports: [],
+  templateUrl: './default-login.component.html',
+  styleUrl: './default-login.component.scss'
+})
+export class DefaultLoginComponent {
+  // @Input() title: string = "";
+  // @Input() primaryBtnText: string = "";
+  // @Input() secondaryBtnText: string = "";
+}
